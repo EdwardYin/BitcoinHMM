fbaae53e6eefa0bd26e5c55fed25c2f6c4c68f12
Unknown <3dward.yin@gmail.com>
2017-10-09 15:56:10 -0400

    Initial commit
    Created transition matrix logic

---
+++ Markov.py
@@ -0,0 +1,67 @@
+import csv
+import random
+import math
+import pandas as pd
+import numpy as np
+
+# reads this CSV file into a DataFrame
+csvPath = 'C:\\Users\\Edward\\Documents\\Github\\BitcoinHMM\\vwapHourlyBTCUSD.csv'
+myDF = pd.read_csv(csvPath)
+
+def discretizeData(inArray):
+    threshold = .0025
+    # Assume 3 states for now
+    outArray = np.empty(len(inArray)-1)
+    for i in range (0, len(inArray)-1):
+        if float(inArray[i])/float(inArray[i+1]) - 1 < -1*threshold:
+            outArray[i] = int(0)
+        elif float(inArray[i])/float(inArray[i+1]) - 1 > threshold:
+            outArray[i] = int(2)
+        else:
+            outArray[i] = int(1)
+    return outArray
+
+def createTransitionMatrix(inArray, keyLength, numStates):
+    #inArray is an array of discretized Data, 0 through n for number of states
+    if numStates < 2:
+        return "Must have at least 2 states in transition matrix"
+    if keyLength < 1:
+        return "Key must be integer >= 1"
+
+    rows = pow(numStates,keyLength)
+    columns = numStates
+    tMatrix = np.empty((rows, columns))
+    countMatrix = np.empty((rows, columns))
+    tMatrix.fill(0)
+    countMatrix.fill(0)
+
+    maxLen = len(inArray) - keyLength - 1
+    for i in range(0,maxLen):
+        # for each starting point, first n states go into key
+        # concatenate into string the first n states into string
+        keyStr = ""
+        for j in range(0, keyLength):
+            keyStr = keyStr + str(int(inArray[i+j]))
+            if i<5:
+                print("string: " + keyStr)
+
+        # turn this into binary for array index
+        index = int(keyStr, numStates)
+
+        #debug print
+        if i<5:
+            print("index: " + str(index))
+
+        countMatrix[index][int(inArray[i+keyLength])] = countMatrix[index][int(inArray[i+keyLength])] + 1
+
+    #populate transition matrix using the count matrix
+    for i in range(0, rows):
+        for j in range(0, columns):
+            tMatrix[i][j] = countMatrix[i][j]/np.sum(countMatrix[i])
+
+    return tMatrix
+
+dArray = discretizeData(myDF['Vwap'])
+endMatrix = createTransitionMatrix(dArray, 3, 3)
+print(endMatrix)
+# createTransitionMatrix(myDF, 3)
